1881a2d55b55ceaf52f83dff0eebb1512882bccc
margarinehound <margarineHoundusers.noreply.github.com>
2019-09-09 01:03:33 -0400

    update

--- selection_sort.py
+++ selection_sort.py
@@ -3,6 +3,25 @@
 def search(lst):
 
     length = len(lst)
-    for n in range(length):
-        smallest = n
-        for k in range(lst[n+1]):
+    for n, idx in enumerate(lst):
+        smallest = idx
+        small_idx = n 
+        #print(n)
+        for k, idx2 in enumerate(lst[n+1:]):
+            if idx2 < idx:
+                smallest = idx2
+                small_idx = k+n+1
+        del lst[small_idx]
+        lst.insert(0,smallest)
+        return lst
+
+
+def main():
+   array = []
+   array.extend(range(30,1, -1))
+   print(array)
+   result = search(array)
+   print(result)
+
+if __name__ == '__main__':
+    main()
