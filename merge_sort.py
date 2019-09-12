7124a71599d0df8ae12e502ddd7b6333e83894cb
margarineHound <margarineHound@users.noreply.github.com>
2019-09-12 02:21:12 -0400

    merge_sort incomplete, needs further fine-tuning

--- merge_sort.py
+++ merge_sort.py
@@ -1,2 +1,42 @@
-def merge_sort(lst):
-    if len(lst) > 1:
+def merge_sort(*args):
+    temp1 = None
+    temp2 = None
+    op = None
+    templist = args[0]
+    l = []
+
+    if len(templist) > 1:
+        for i in templist:
+            m = int(len(templist)/2)
+            op = merge_sort(templist[:m], templist[m+1:])
+    else:
+        return [templist]
+    if len(op[0])>len(op[1]):
+        temp1 = 0
+        temp2 = 1
+    else:
+        temp1 = 1
+        temp2 = 0
+    while op[temp1]:
+        if op[temp1][0] > op[temp2][0]:
+            l.append(op[temp2][0])
+            del op[temp2][0]
+            if not op[temp2]:
+                l.extend(temp1)
+                return l
+            else:
+                continue
+        else:
+            l.append(op[temp1][0])
+            del op[temp1][0]
+            if not op[temp1]:
+                l.extend(temp2)
+                return l
+            else:
+                continue
+
+    return l
+def main():
+    pass
+if __name__ == '__main__':
+    main()
--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 from selection_sort import select_sort
+from merge_sort import merge_sort
 
 def main():
    array = []
    array.extend(range(30,0, -1))
    print(array)
-   result = select_sort(array)
+   result = merge_sort(array)
    print(result)
 
 if __name__ == '__main__':
