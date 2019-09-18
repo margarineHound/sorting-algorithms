0a04bf7478f0d73698228aca4e57d4aacef86d99
margarineHound <margarineHound@users.noreply.github.com>
2019-09-18 15:47:13 -0400

    fixed merge_sort

--- merge_sort.py
+++ merge_sort.py
@@ -1,36 +1,31 @@
 def merge_sort(*args):
-    temp1 = None
-    temp2 = None
-    op = None
+    # temp1 = None
+    # temp2 = None
+    # op = None
     templist = args[0]
     l = []
 
     if len(templist) > 1:
-        for i in templist:
-            m = int(len(templist)/2)
-            op = merge_sort(templist[:m], templist[m+1:])
+        m = int(len(templist)/2)
+        op1 = merge_sort(templist[:m])
+        op2 = merge_sort(templist[m:])
     else:
-        return [templist]
-    if len(op[0])>len(op[1]):
-        temp1 = 0
-        temp2 = 1
-    else:
-        temp1 = 1
-        temp2 = 0
-    while op[temp1]:
-        if op[temp1][0] > op[temp2][0]:
-            l.append(op[temp2][0])
-            del op[temp2][0]
-            if not op[temp2]:
-                l.extend(temp1)
+        return templist
+    lentot = len(op1) + len(op2)
+    for i in range(lentot):
+        if op1[0] > op2[0]:
+            l.append(op2[0])
+            del op2[0]
+            if not op2:
+                l.extend(op1)
                 return l
             else:
                 continue
         else:
-            l.append(op[temp1][0])
-            del op[temp1][0]
-            if not op[temp1]:
-                l.extend(temp2)
+            l.append(op1[0])
+            del op1[0]
+            if not op1:
+                l.extend(op2)
                 return l
             else:
                 continue
--- test.py
+++ test.py
@@ -4,6 +4,8 @@ from merge_sort import merge_sort
 def main():
    array = []
    array.extend(range(30,0, -1))
+   # array = [8,7,6,5,4,3,2,1]
+   array.extend([1,12,12,12,12,12,12,12,12,13,24,24,24])
    print(array)
    result = merge_sort(array)
    print(result)
